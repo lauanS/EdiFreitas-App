fc6a420671a5c82f6ebdcf5adc2a0187a8e16ba8
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-11-22 12:08:57 -0300

    heroku deploy

---
+++ build/precache-manifest.ea53166a477d1b434aa514564b1aae57.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733b1b3d64e7b3cf0703b245b9383c5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada437dd18eca90f04f2",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "1d1fa9b2b11ca5fca012",
+    "url": "/static/css/main.25b07b00.chunk.css"
+  },
+  {
+    "revision": "ada437dd18eca90f04f2",
+    "url": "/static/js/2.c1919f37.chunk.js"
+  },
+  {
+    "revision": "1d1fa9b2b11ca5fca012",
+    "url": "/static/js/main.eba688ea.chunk.js"
+  },
+  {
+    "revision": "f649b18296e62814dce0",
+    "url": "/static/js/runtime-main.2c7d9cb5.js"
+  },
+  {
+    "revision": "f1cced986fc3a2fd79f6edd10d3ff992",
+    "url": "/static/media/ong_01.f1cced98.jpg"
+  },
+  {
+    "revision": "20101ebfd1ad0d7331afa9906a052ba3",
+    "url": "/static/media/ong_02.20101ebf.jpg"
+  },
+  {
+    "revision": "8dac4f9481ebc9538ea0aeb3c25cc5de",
+    "url": "/static/media/ong_03.8dac4f94.jpg"
+  },
+  {
+    "revision": "ebdcfa00c47eaa361e1693dca0662a81",
+    "url": "/static/media/ong_logo.ebdcfa00.jpg"
+  }
+]);
