30545266ada09f46325d8085e679835ee3651213
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-11-24 11:47:14 -0300

    Alteracoes no footer, nao esta completo, falta mudar as cores

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Row, Container } from 'react-bootstrap';
 import './styles.css';
-import { SocialIcon } from 'react-social-icons';
+import Instagram from '@material-ui/icons/Instagram';
+import Facebook from '@material-ui/icons/Facebook';
 
 export default class Footer extends React.Component{
   render(){
@@ -9,12 +10,12 @@ export default class Footer extends React.Component{
       <footer class="text-muted shadow">
         <Container>
           <Row className="justify-content-md-center">
-            <a href="http://facebook.com/ongedifreitas" target="_blank">
-              <SocialIcon network="facebook" />  Facebook
-              </a>
-              <a href="http://instagram.com/ongedifreitas" target="_blank">
-                <SocialIcon network="instagram" />  Instagram
-                </a>
+            <a href="http://facebook.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
+              <Facebook className="icons" fontSize="50px" /> Facebook
+            </a>
+            <a href="http://instagram.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
+              <Instagram className="icons" /> Instagram
+            </a>
           </Row>
           <br/>
           <Row className="center justify-content-md-center">
