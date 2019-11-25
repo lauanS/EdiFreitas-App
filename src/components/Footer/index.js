46a2eb6ddc9c54b3b7e4846b6b6e75f54fda9471
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-11-25 19:58:30 -0300

    Alteracoes design footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { Row, Container } from 'react-bootstrap';
-import './styles.css';
+import './styles.scss';
 import Instagram from '@material-ui/icons/Instagram';
 import Facebook from '@material-ui/icons/Facebook';
 
 export default class Footer extends React.Component{
   render(){
     return (
-      <footer class="text-muted shadow">
+      <footer className="foot">
         <Container>
           <Row className="justify-content-md-center">
-            <a href="http://facebook.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
-              <Facebook className="icons" fontSize="50px" /> Facebook
+            <a className="foot__link" href="http://facebook.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
+              <Facebook className="foot__icons" fontSize="50px" /> Facebook
             </a>
-            <a href="http://instagram.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
-              <Instagram className="icons" /> Instagram
+            <a className="foot__link" href="http://instagram.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
+              <Instagram className="foot__icons" /> Instagram
             </a>
           </Row>
           <br/>
-          <Row className="center justify-content-md-center">
+          <Row className="foot__center justify-content-md-center">
             <p>ONG Edi Freitas - Rua Três, 12, 18071-303. Sorocaba - São Paulo - Brasil.</p>
           </Row>
-          <Row className="center justify-content-md-center">
+          <Row className="foot__center justify-content-md-center">
             <p className="copyright pull-right">  &copy; {" Copyright "}{new Date().getFullYear()}{" EdiFreitas - All Rights Reserved"} </p>
           </Row>
         </Container>
