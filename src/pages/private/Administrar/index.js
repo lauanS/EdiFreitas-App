f40022db52613975553b2f3d0772cc006ee69856
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-11-27 00:45:52 -0300

    Alterando a pagina inicial do admin, mensagem de bem vindo

--- src/pages/private/Administrar/index.js
+++ src/pages/private/Administrar/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.scss';
-import CadPessoa from '../../../components/CadastroResponsavel/index';
 import Sidebar from '../../../components/Sidebar/index';
 
 export default class Administrar extends React.Component{
-    render(){
-        return (
-          <>
-          <Sidebar titulo="Edi Freitas" ativo={-1}/>
-          
-          <div className="cad">
-            <CadPessoa />   
-          </div>
-          </>
-        );
-      }
+  render(){
+    return (
+      <>
+        <Sidebar titulo="Edi Freitas" ativo={-1}/>
+        
+        <div className="cad">
+          <h4>Bem vindo ao sistema de gerenciamento da ONG Edi Freitas.</h4>
+  
+          <h6>&thinsp; &thinsp;Caso esteja em um dispositivo móvel, possivelmente as opções estão ocultas no menu,
+          podendo ser encontrado na barra superior da tela
+          </h6>
+
+          <h6>&thinsp; &thinsp;Caso esteja utilizando outro dispositivo como um computador ou notebook,
+          as opções podem ser encontradas no lado esquerdo a tela
+          </h6>
+        </div>
+      </>
+    );
+  }
 
 }
