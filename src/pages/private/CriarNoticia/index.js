61cd93bd3b89b412766b42d29ed23eb4a0a9c53a
lauanS <lauan.s.souza@gmail.com>
2019-11-28 19:24:07 -0300

    Tela de edição de notícias + suas dependências

--- src/pages/private/CriarNoticia/index.js
+++ src/pages/private/CriarNoticia/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './styles.scss';
+import { Form } from 'react-bootstrap';
 import Sidebar from '../../../components/Sidebar/index';
+import TextEditor from '../../../components/EditorDeTexto/index'
+import DadosNoticia from '../../../components/DadosNoticia/index'
 
 export default class CriarNoticia extends React.Component{
     render(){
         return (
           <>
           <Sidebar titulo="Criar notícia" ativo={6}/>
+          <div className='newsContent'>
+            <Form>
+              <DadosNoticia />
+              <div className='newsEditor'>
+                <TextEditor />   
+              </div>
+            </Form>
+
+          </div>
+
           </>
         );
       }
