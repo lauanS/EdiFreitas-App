42f481cce8ddfeea9bd3a83aee590141bac95f2b
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-12-07 18:14:13 -0300

    alteracoes no consultar pessoas, adicionando um input para digitar o nome

--- src/components/ListarPessoas/index.js
+++ src/components/ListarPessoas/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles.scss';
 import ModalCard from './modalCard';
 import nozaki from '../../assets/nozaki.jpg';
@@ -7,10 +7,31 @@ import cad from '../../assets/cad.jpg';
 import crack from '../../assets/crack.jpg';
 
 export default function ListarPessoas() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = e => {
+    setName("");
+  };
+
   return (
     <>
-    <div>
+    <div className="header">
+      <form className="header__search-bar" onSubmit={(e) => e.preventDefault()}>
+        <label className="header__label">Nome da pessoa</label>
+        <div className="header__input-button">
+          <input
+            type="text"
+            className="header__search-input"
+            placeholder="Ex: João"
+            onChange={e => setName(e.target.value)}
+            value={name} />
+          <button type="submit" className="header__search-button" onClick={handleSubmit}>
+            Pesquisar
+          </button>
+        </div>
+      </form>
     </div>
+
     <div className="resultadosPessoas"> 
       <ModalCard crianca={false} dtNasc={"02/02/1998"} photo={nozaki} name="Leonardo Seiji Nozaki nozaki nozaki nozaki" idade={21}/>
       <ModalCard crianca={true} dtNasc={"02/02/1998"} numCalcado={38} photo={stein} name="Leonardo Stein Campelo" idade={20}/>
