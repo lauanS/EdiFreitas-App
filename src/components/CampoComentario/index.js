01bb7ce651fdf941d6e14023500ef06958e8b671
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-12-09 00:48:34 -0300

    Alteracoes necessarias no campo comentario

--- src/components/CampoComentario/index.js
+++ src/components/CampoComentario/index.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 import {Form, Row, Col} from 'react-bootstrap';
 
 
-export default function CampoComentario(){
+export default function CampoComentario(props){
+  const {setComentario} = props;
+  const [valited, setValidated] = useState(false);
+
+  const onChange = e => {
+   setComentario(e.value);
+   if(e.value.length > 0){
+     setValidated(true);
+   }
+   else{
+     setValidated(false);
+   }
+  }
 
   return (
-      <Form.Group as={Row} controlId="formGroupComment">
-        <Form.Label column sm={2}>
-          Comentário
-        </Form.Label>
-        <Col>
-          <Form.Control as="textarea" rows="3"/>
-        </Col>
-      </Form.Group>
+    <Form.Group as={Row} controlId="formGroupComment">
+      <Form.Label column sm={3}>
+        Comentário
+      </Form.Label>
+      <Col sm={9}>
+        <Form.Control 
+          onChange={e => onChange(e.target)}
+          as="textarea" 
+          isValid={valited}
+          rows="3" 
+          placeholder="Algum comentário sobre a criança"/>
+      </Col>
+    </Form.Group>
   );
 }
