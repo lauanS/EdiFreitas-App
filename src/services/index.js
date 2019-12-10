1b9f0bc10849e06d2d00b1b59c6d4889c041aa62
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-12-10 15:09:58 -0300

    Colocando endpoint correto no getPeople

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const getPeople = () => {
-    return axios.get('http://edi-freitas.herokuapp.com/');
+    return axios.get('http://edi-freitas.herokuapp.com/api/responsaveis');
 }
