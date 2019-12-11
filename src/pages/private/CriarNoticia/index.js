85ed0a7424617963d009bec38f5cbd330522aef2
lauanS <lauan.s.souza@gmail.com>
2019-12-11 19:26:35 -0300

    Função de exibir/ocultar campos da notícia

--- src/pages/private/CriarNoticia/index.js
+++ src/pages/private/CriarNoticia/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './styles.scss';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Collapse } from 'react-bootstrap';
 import Sidebar from '../../../components/Sidebar/index';
 import TextEditor from '../../../components/EditorDeTexto/index'
 import DadosNoticia from '../../../components/DadosNoticia/index'
@@ -10,6 +10,9 @@ export default function CriarNoticia(){
   const [title, setTitle] = useState("");
   const [subtitle, setSubtitle] = useState("");
   const [text, setText] = useState("");
+
+  const [open, setOpen] = useState(true);
+
 
   const handleSubmit = e => {
     console.log('Title: ', title);
@@ -32,9 +35,27 @@ export default function CriarNoticia(){
     <Sidebar titulo="Criar notícia" ativo={6} key={"sidebar"}/>
     <div className='newsContent'>
       <Form onSubmit={handleSubmit} >
-        <DadosNoticia setTitle={setTitle} setSubtitle={setSubtitle} />
+        <Collapse in={open}>
+          <div id="fade-fields">
+            <DadosNoticia setTitle={setTitle} setSubtitle={setSubtitle} />
+          </div>
+        </Collapse>
+
+        <div className='divBtnHide'>
+          <Button
+            className='centerButton'
+            onClick={() => setOpen(!open)}
+            aria-controls="fade-fields"
+            aria-expanded={open}
+            variant="link"
+          >
+            Esconder/exibir campo de título e subtítulo
+          </Button>
+        </div>
+
+
         <div className='newsEditor'>
-          <TextEditor handleChange={handleChildChange}/>   
+          <TextEditor handleChange={handleChildChange}/>      
         </div>
 
         <Button type="submit" block>Cadastrar</Button>
