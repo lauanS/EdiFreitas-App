5dba4c81ce9c24774f0b9af30db722893c2ac498
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-12-30 20:39:07 -0300

    Alteracoes no services e validated

--- src/services/index.js
+++ src/services/index.js
@@ -1,16 +1,21 @@
 import axios from "axios";
 
-export const getPeople = () => {
-    return axios.get('https://edi-freitas.herokuapp.com/api/responsaveis');
+export const getResponsaveis = () => {
+  return axios.get('https://edi-freitas.herokuapp.com/api/responsaveis');
 }
 
 export const postResponsavel = (responsavel) => {
-    axios.post('https://edi-freitas.herokuapp.com/api/responsaveis', responsavel).then(res => {
-        console.log(res);
-        console.log(res.data);
-      });
+  axios.post('https://edi-freitas.herokuapp.com/api/responsaveis', responsavel).then(res => {
+    console.log(res);
+    console.log(res.data);
+  });
 }
 
 export const delPeople = (id) => {
-    return axios.delete('http://edi-freitas.herokuapp.com/api/responsaveis/' + id);
+  return axios.delete('http://edi-freitas.herokuapp.com/api/responsaveis/' + id);
 }
+
+export const getCriancas = () => {
+  return axios.get('http://edi-freitas.herokuapp.com/api/criancas');
+}
+
