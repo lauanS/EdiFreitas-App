47c7b1d7bfdd9c6c8cb83fadd1ae99e7d0a4e9d8
lauanS <lauan.s.souza@gmail.com>
2020-01-19 16:58:06 -0300

    Alterações no layout

--- src/components/Endereco/index.js
+++ src/components/Endereco/index.js
@@ -6,6 +6,8 @@ import {Form, Row, Col} from 'react-bootstrap';
 
 
 export default function Endereco(props){
+  const [invalidatedNumero, setInvalidatedNumero] = useState(false);
+
   /* Formata o CEP */
   const onChangeCep = e => {
     let value = e.target.value;
@@ -23,51 +25,97 @@ export default function Endereco(props){
     e.target.value = newValue;           
   }
 
+    /* Verifica o número */
+    const onChangeNumero = e => {
+      let value = e.target.value;
+      let re = /[^\d]+/gm;
+
+      if(value.match(re)){
+        setInvalidatedNumero(true);
+      }          
+      else{
+        setInvalidatedNumero(false);
+      }
+    }
+
   return (
     <>
-    <Form.Group as={Row} controlId="formGroupCpf">
-      <Form.Label column sm={2}>Endereço</Form.Label>
+    <Form.Group as={Row} controlId="formLogradouro">
+      <Form.Label column sm={2}>
+        Logradouro
+      </Form.Label>
       <Col sm={8}>
-
-        <Form.Group controlId="formLogradouro">
-          <Form.Label>Logradouro</Form.Label>
-          <Form.Control placeholder="Rua/Avenidade/Estrada"/>
-        </Form.Group>
-
-
-        <Form.Row>
-          <Form.Group as={Col} sm="3" controlId="formNumero">
-            <Form.Label>Número</Form.Label>
-            <Form.Control />
-          </Form.Group>
-
-          <Form.Group as={Col} className="CEP-cad" controlId="formCEP">
-            <Form.Label>CEP</Form.Label>
-            <Form.Control
-              placeholder="Digite apenas números"
-              onChange={onChangeCep}
-            />
-          </Form.Group>
-        </Form.Row>
-
-
-        <Form.Group controlId="formBairro">
-          <Form.Label>Bairro</Form.Label>
-          <Form.Control />
-        </Form.Group>
-
-        <Form.Group controlId="formCidade">
-          <Form.Label>Cidade</Form.Label>
-          <Form.Control />
-        </Form.Group>
-
-        <Form.Group controlId="formEstado">
-          <Form.Label>Estado</Form.Label>
-          <Form.Control />
-        </Form.Group>
-
+        <Form.Control 
+          placeholder="Rua/Avenidade/Estrada"
+        />
       </Col>
     </Form.Group>
+
+
+    
+    <Form.Group as={Row} sm="3" controlId="formNumero">
+      <Form.Label  column sm={2}>
+        Número
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control 
+          isInvalid={invalidatedNumero}
+          placeholder="Digite apenas números"
+          onChange={onChangeNumero}
+        />
+      </Col>
+    </Form.Group>
+
+    <Form.Group as={Row}  controlId="formCEP">
+      <Form.Label  column sm={2}>
+        CEP
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control
+          className="CEP-cad"
+          placeholder="Digite apenas números"
+          onChange={onChangeCep}
+        />
+      </Col>     
+    </Form.Group>
+    
+
+
+    <Form.Group as={Row} controlId="formBairro">
+      <Form.Label column sm={2}>
+        Bairro
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control
+          placeholder="Ex: Jd. Neptune"
+        />
+      </Col>
+    </Form.Group>
+
+    <Form.Group as={Row} controlId="formCidade">
+      <Form.Label column sm={2}>
+        Cidade
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control
+          value="Sorocaba"        
+        />
+      </Col>
+    </Form.Group>
+
+    <Form.Group as={Row} controlId="formEstado">
+      <Form.Label column sm={2}>
+        Estado
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control 
+          value="São Paulo"
+        />
+      </Col>
+      
+    </Form.Group>
+    
+
     
     </>
   );
