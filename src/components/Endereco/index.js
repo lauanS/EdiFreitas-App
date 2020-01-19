fdc191da75bdbcc2054fc81ddddabc04b37389ed
lauanS <lauan.s.souza@gmail.com>
2020-01-19 18:04:03 -0300

    Mudanças na aparência dos inputs

--- src/components/Endereco/index.js
+++ src/components/Endereco/index.js
@@ -21,50 +21,61 @@ export default function Endereco(props){
                         return arg1 + arg2 ;
                       }
                         
-                    });;
+                    });
     e.target.value = newValue;           
   }
 
-    /* Verifica o número */
-    const onChangeNumero = e => {
-      let value = e.target.value;
-      let re = /[^\d]+/gm;
+  /* Verifica o número */
+  const onChangeNumero = e => {
+    let value = e.target.value;
+    let re = /[^\d]+/gm;
 
-      if(value.match(re)){
-        setInvalidatedNumero(true);
-      }          
-      else{
-        setInvalidatedNumero(false);
-      }
+    if(value.match(re)){
+      setInvalidatedNumero(true);
+    }          
+    else{
+      setInvalidatedNumero(false);
     }
+  }
 
   return (
     <>
     <Form.Group as={Row} controlId="formLogradouro">
       <Form.Label column sm={2}>
-        Logradouro
+        Logradouro *
       </Form.Label>
       <Col sm={8}>
         <Form.Control 
+          required
+          type="text" 
           placeholder="Rua/Avenidade/Estrada"
+          className="cadastro-inputText"
         />
+        <Form.Control.Feedback type="invalid">
+          Campo obrigatório.
+        </Form.Control.Feedback>
       </Col>
     </Form.Group>
 
 
-    
     <Form.Group as={Row} sm="3" controlId="formNumero">
       <Form.Label  column sm={2}>
-        Número
+        Número *
       </Form.Label>
       <Col sm={8}>
         <Form.Control 
+          required
+          className="num-endereco"
           isInvalid={invalidatedNumero}
           placeholder="Digite apenas números"
           onChange={onChangeNumero}
         />
+        <Form.Control.Feedback type="invalid">
+          Campo obrigatório, digite apenas números.
+        </Form.Control.Feedback>
       </Col>
     </Form.Group>
+
 
     <Form.Group as={Row}  controlId="formCEP">
       <Form.Label  column sm={2}>
@@ -72,51 +83,66 @@ export default function Endereco(props){
       </Form.Label>
       <Col sm={8}>
         <Form.Control
-          className="CEP-cad"
+          className="num-endereco"
           placeholder="Digite apenas números"
           onChange={onChangeCep}
-        />
+        />        
       </Col>     
     </Form.Group>
     
 
-
     <Form.Group as={Row} controlId="formBairro">
       <Form.Label column sm={2}>
-        Bairro
+        Bairro *
       </Form.Label>
       <Col sm={8}>
         <Form.Control
+          required
+          type="text" 
           placeholder="Ex: Jd. Neptune"
+          className="cadastro-inputText"
         />
+        <Form.Control.Feedback type="invalid">
+          Campo obrigatório.
+        </Form.Control.Feedback>
       </Col>
     </Form.Group>
+
 
     <Form.Group as={Row} controlId="formCidade">
       <Form.Label column sm={2}>
-        Cidade
+        Cidade *
       </Form.Label>
       <Col sm={8}>
         <Form.Control
-          value="Sorocaba"        
+          required
+          defaultValue="Sorocaba"     
+          className="cadastro-inputText"   
         />
+        <Form.Control.Feedback type="invalid">
+          Campo obrigatório.
+        </Form.Control.Feedback>
       </Col>
     </Form.Group>
+
 
     <Form.Group as={Row} controlId="formEstado">
       <Form.Label column sm={2}>
-        Estado
+        Estado *
       </Form.Label>
       <Col sm={8}>
         <Form.Control 
-          value="São Paulo"
+          required
+          defaultValue="São Paulo"
+          className="cadastro-inputText"
         />
+        <Form.Control.Feedback type="invalid">
+          Campo obrigatório.
+        </Form.Control.Feedback>
       </Col>
-      
     </Form.Group>
     
 
-    
     </>
   );
 }
