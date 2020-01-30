2c117d4c712593d88819092dfe5da46d63c4ff14
lauanS <lauan.s.souza@gmail.com>
2020-01-30 19:27:04 -0300

    Atualização das imagens temporárias

--- src/components/ListarEventos/events.js
+++ src/components/ListarEventos/events.js
@@ -2,7 +2,7 @@ export default function getEvents(){
   const n1 = { 
     title:"Novo title",
     subtitle:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente minus id voluptatibus atque nam voluptatum illum temporibus repudiandae,",
-    urlImg:"https://steamuserimages-a.akamaihd.net/ugc/486769343081008872/C1C2C608F78907DCBFBD78ADFE683C942E7B9C25/",
+    urlImg:"https://static.zerochan.net/Sajou.Yukimi.full.1669795.jpg",
     firstFooter:"25/01/2020",
     lastFooter:"Não é no RU",
   }
@@ -10,7 +10,7 @@ export default function getEvents(){
   const n2 = {
     title:"Lorem ipsum dolor",
     subtitle:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente minus id voluptatibus atque nam voluptatum illum temporibus repudiandae,",
-    urlImg:"https://cdn.myanimelist.net/r/360x360/images/characters/15/266029.jpg?s=029bb39a4d062e862fbbc59bd9dea268",
+    urlImg:"https://i0.wp.com/www.pontojao.com.br/wp-content/uploads/2018/03/violet-capa.png?fit=500%2C500",
     firstFooter:"25/01/2020",
     lastFooter:"Não é no RU",
    }
@@ -18,7 +18,7 @@ export default function getEvents(){
   const n3 = { 
     title:"Trabalho de Extensão para a ONG EdiFreitas Extensão para a ONG EdiFreitas",
     subtitle:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente minus id voluptatibus atque nam voluptatum illum temporibus repudiandae,",
-    urlImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSGSchNBJSYBWUARzgM2YisE5S9_Ew8LSyblcHTg_sCRf38-ApP",
+    urlImg:"https://data.whicdn.com/images/106478614/original.png",
     firstFooter:"20/01/2020",
     lastFooter:"Não é no RU",
   }
@@ -26,7 +26,7 @@ export default function getEvents(){
   const n4 = {  
     title:"Lorem ipsum dolor",
     subtitle:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente minus id voluptatibus atque nam voluptatum illum temporibus repudiandae,",
-    urlImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSkYn64tYtWf04OeSY2Wnapanr_BzXLNtUbukVcbzEP7Ox04wls",
+    urlImg:"https://i.pinimg.com/originals/29/ba/56/29ba562561db716f60de27114ca54dda.png",
     firstFooter:"20/01/2020",
     lastFooter:"Não é no RU",      
   }
@@ -34,7 +34,7 @@ export default function getEvents(){
   const n5 = {  
     title:"Lorem ipsum dolor",
     subtitle:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente minus id voluptatibus atque nam voluptatum illum temporibus repudiandae,",
-    urlImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0h6YYldvKZUH9MQu3WWhxpDGh9Uvu8mNafg-GGaQyvHcdK_ca",
+    urlImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQpUBCd5C3ndMpgZEOSPUS3RraI6jG9xJUFTnCnIezFY64dzvFC",
     firstFooter:"25/01/2020",
     lastFooter:"Não é no RU",
   }
@@ -42,7 +42,7 @@ export default function getEvents(){
   const n6 = { 
     title:"Lorem ipsum dolor",
     subtitle:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente minus id voluptatibus atque nam voluptatum illum temporibus repudiandae,",
-    urlImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ9ZMs6-RUMHVgbRhRwZh8KhRZizU55MwhREo75SwFNSthHbGWC",
+    urlImg:"https://vignette.wikia.nocookie.net/sao-abridged/images/8/88/Asuna_SAO.png/revision/latest?cb=20190905123928",
     firstFooter:"25/01/2020",
     lastFooter:"Não é no RU",
   }
