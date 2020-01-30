bbba7a3bbd6d48657daa7120ea1a602e36313edf
lauanS <lauan.s.souza@gmail.com>
2020-01-30 19:06:35 -0300

    Atualizando as páginas de consultas

--- src/pages/private/ConsultarNoticias/index.js
+++ src/pages/private/ConsultarNoticias/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Sidebar from '../../../components/Sidebar/index';
 import ListarNoticias from "../../../components/ListarNoticias";
+
 import './styles.scss';
 
 export default class ConsultarNoticias extends React.Component{
