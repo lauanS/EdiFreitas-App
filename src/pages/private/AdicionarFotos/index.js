cf622b940c54d1884ddd78b8dddefabca197c10e
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-02-06 18:07:01 -0300

    criancao do componente inicial de campoImagem e importando ele na pagina

--- src/pages/private/AdicionarFotos/index.js
+++ src/pages/private/AdicionarFotos/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './styles.scss';
 import Sidebar from '../../../components/Sidebar/index';
-
+import CampoImagem from '../../../components/CampoImagem/index'
 export default class AdicionarFotos extends React.Component{
     render(){
         return (
           <>
           <Sidebar titulo="Adicionar fotos" ativo={8}/>
+
+          <div className="AdicionarFotos">
+            <CampoImagem />   
+          </div>
           </>
         );
       }
