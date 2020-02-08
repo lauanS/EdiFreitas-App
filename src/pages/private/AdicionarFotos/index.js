4779f43d61670a33228f1a235a6f3965c27f21ef
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-02-08 17:16:25 -0300

    Ops, correcao necessaria no adicionar fotos

--- src/pages/private/AdicionarFotos/index.js
+++ src/pages/private/AdicionarFotos/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './styles.scss';
 import Sidebar from '../../../components/Sidebar/index';
-import CampoImagem from '../../../components/CampoImagem/index'
+
 export default class AdicionarFotos extends React.Component{
     render(){
         return (
           <Sidebar titulo="Adicionar fotos" ativo={8}>
-            <CampoImagem />
+
           </Sidebar>
         );
       }
