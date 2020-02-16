f8a7c27331458114803c9a8c83eb049305359ee8
Lucas Sampaio <lucas.souza@dcomp.sor.ufscar.br>
2020-02-16 19:05:52 -0300

    filtros na consultas de pessoas

--- src/components/ListarPessoas/index.js
+++ src/components/ListarPessoas/index.js
@@ -8,6 +8,7 @@ import Snackbar from '../Snackbars';
 
 import axios from "axios";
 import {getResponsaveis, getCriancas} from '../../services';
+import { idade } from "../../assist";
 
 export default class extends React.Component {
   constructor(props) {
@@ -23,6 +24,9 @@ export default class extends React.Component {
       alertErrorUpdate: false, 
       responsaveis: [],
       criancas: [],
+      idadeMax: 200,
+      idadeMin: 0,
+      exibir: 0
     };
   }
 
@@ -57,13 +61,11 @@ export default class extends React.Component {
     e.preventDefault();
   }
 
-  searchPeople(list, search) {
-    if (search === '') {
-      return list;
-		}
-
-    return Array.isArray(list) ? list.filter(person =>
-      person.nome.toLowerCase().indexOf(search.toLowerCase()) !== -1 )
+  searchPeople(list, search, idadeMax, idadeMin) {
+    return Array.isArray(list) ? list.filter(person => 
+      person.nome.toLowerCase().indexOf(search.toLowerCase()) !== -1 && 
+      idade(person.dataNascimento) < idadeMax  && 
+      idade(person.dataNascimento) > idadeMin)
       : [];
   }
 
@@ -103,11 +105,35 @@ export default class extends React.Component {
     this.setState({ ...this.state, alertErrorUpdate: b }); 
   }
 
+
+  updateIdadeMax(){
+    let b = document.getElementById("formGroupIdadeMax")
+
+    if(b.value === '')
+      this.setState({ ...this.state, idadeMax: 200 });
+    else
+      this.setState({ ...this.state, idadeMax: b.value });
+  }
+
+  updateIdadeMin(){
+    let b = document.getElementById("formGroupIdadeMin")
+
+    if(b.value === '')
+      this.setState({ ...this.state, idadeMin: 0 });
+    else
+      this.setState({ ...this.state, idadeMin: b.value });
+  }
+
+  updateExibir(b){
+    this.setState({ ...this.state, exibir: Number(b.target.value) });
+    b.preventDefault();
+  }
+
   render(){
-    const { responsaveis, criancas, isLoading, errors, search } = this.state;
+    const { responsaveis, criancas, isLoading, errors, search, idadeMax, idadeMin, exibir } = this.state;
     const {alertSuccessDeletar, alertErrorDeletar, alertSuccessUpdate, alertErrorUpdate} = this.state;
-    const filteredResponsaveis = this.searchPeople(responsaveis, search) || [];
-    const filteredCriancas = this.searchPeople(criancas, search) || [];
+    const filteredResponsaveis = exibir === 0 || exibir === 2 ? this.searchPeople(responsaveis, search, idadeMax, idadeMin) || [] : [];
+    const filteredCriancas = exibir === 0 || exibir === 1 ? this.searchPeople(criancas, search, idadeMax, idadeMin) || [] : [];
 
     return (
       <>
@@ -120,19 +146,63 @@ export default class extends React.Component {
       {isLoading ? <Loader type="dots" /> : errors ? "Houve algum problema" :
       <>
       <Form autoComplete="off">
-        <Form.Group as={Row} controlId="formGroupName">
-          <Form.Label column sm={2} className="listarPessoas__label">
-            Nome da pessoa
-          </Form.Label>
-          <Col sm={8} className="listarPessoas__inputText">
-            <Form.Control 
-              type="text" 
-              placeholder="Ex: Leonardo dos Santos Sampaio" 
-              onChange={this.updateSearch.bind(this)}
-              value={this.state.search}
-            />
+        <Row>
+          <Form.Group controlId="formGroupName">
+            <Form.Label className="listarPessoas__label">
+              Nome da pessoa:
+            </Form.Label>
+            <Col sm={8} className="listarPessoas__inputText">
+              <Form.Control 
+                type="text" 
+                placeholder="Ex: Leonardo dos Santos Sampaio" 
+                onChange={this.updateSearch.bind(this)}
+                value={this.state.search}
+              />
+            </Col>
+          </Form.Group>
+          
+          <Col sm="1">
+            <Form.Group controlId="formGroupIdadeMin">
+              <Form.Label >
+                Idade min:
+              </Form.Label> 
+              <Form.Control
+                type="number" 
+                placeholder= "2"
+                onChange = {() => this.updateIdadeMin()}
+              />
+            </Form.Group>
           </Col>
-        </Form.Group>
+
+          <Col sm="1">
+            <Form.Group controlId="formGroupIdadeMax">
+              <Form.Label>
+                Idade max:
+              </Form.Label>
+              <Form.Control
+                type="number" 
+                placeholder= "2"
+                onChange = {() => this.updateIdadeMax()}
+              />
+            </Form.Group>
+          </Col>
+
+          <Col sm="2">
+            <Form.Group controlId="formGroupExibir">
+              <Form.Label>
+                Exibir:
+              </Form.Label>
+              <Form.Control 
+                as="select"
+                onChange={this.updateExibir.bind(this)}
+              >
+                <option value="0">Todas as pessoas</option>
+                <option value="1">Apenas crianças</option>
+                <option value="2">Apenas responsaveis</option>
+              </Form.Control>
+            </Form.Group>
+          </Col>
+        </Row>
       </Form>
       
       {filteredResponsaveis.length === 0 && filteredCriancas.length === 0 ? 
