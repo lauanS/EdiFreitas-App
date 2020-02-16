5d87dd0127f038c008b396e56de224a97dfbf8be
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-02-16 14:25:13 -0300

    alterando botao de submit no cadastro crianca

--- src/components/CadastroCrianca/index.js
+++ src/components/CadastroCrianca/index.js
@@ -192,7 +192,7 @@ export default function CadastroCrianca(){
 
     <label className="CadastroCrianca__descricao">É obrigatório o preenchimento de campos com * (Asterisco) no título, é opcional quando não possuem o asterisco</label>
     
-    <Form onSubmit={handleSubmit} noValidate autoComplete="off">
+    <Form noValidate autoComplete="off">
       <CamposPessoa nome={nomeCompleto} onChangeNome={onChangeNome} valNome={validatedNomeCompleto} invNome={invalidatedNomeCompleto}
           data={dataNascimento} onChangeData={onChangeData} valData={validatedDataNascimento} invData={invalidatedDataNascimento}
           sexo={sexoPessoa} onChangeSexo={onChangeSexo}
@@ -306,11 +306,7 @@ export default function CadastroCrianca(){
 
       <Comentario validatedComentario={validatedComentario} setValidatedComentario={setValidatedComentario} comentario={comentario} setComentario={setComentario}/>
 
-      <Form.Group as={Row}>
-        <Col sm={{ span: 10, offset: 2 }}>
-          <Button type="submit">Cadastrar</Button>
-        </Col>
-      </Form.Group>
+      <Button className="CadastroCrianca__buttonSubmit" variant="success" onClick={handleSubmit}>Cadastrar criança</Button>
     </Form>
     </>
   );
