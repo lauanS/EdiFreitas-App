4ca215ac62b06da34b0ed02521a0c27da2ca23a1
lauanS <lauan.s.souza@gmail.com>
2020-02-20 19:32:36 -0300

    Refatorando o código

--- src/components/Noticias/Feed/index.js
+++ src/components/Noticias/Feed/index.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import Pagination from 'react-bootstrap/Pagination'
-import PageItem from 'react-bootstrap/PageItem'
-
 import Post from '../Post'
 
 import { getNoticias } from "../../../services";
@@ -12,22 +9,21 @@ import './styles.scss';
 
 export default function Feed(props){
   const [news, setNews] = useState([]);
-  const [items, setItems] = useState([]);
 
   async function loadNews(){
-    const response = await getNoticias();
-    setNews(response.data)
+    try {
+      const response = await getNoticias();
+      setNews(response.data);
+    } catch (error) {
+      console.log(`<ERRO>: ${error}`);
+    }
 
     return;
   }
 
   /* Carregando as notícias */
   useEffect(() => {
-    async function load(){
-      await loadNews();       
-    }   
-    load();
-       
+    loadNews();       
   }, []);
 
 
