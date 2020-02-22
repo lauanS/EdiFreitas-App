ba6e875bf3bc0e32cfcd2396e95f0d23318fe96d
lauanS <lauan.s.souza@gmail.com>
2020-02-22 01:44:30 -0300

    Visualização do texto da notícias +ajustes visuais

--- src/components/Noticias/View/index.js
+++ src/components/Noticias/View/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import '../styles.scss';
+import Interweave  from 'interweave';
+import './styles.scss';
 
 export default function View(props){
   const { obj } = props;
 
   return (
     <>  
-    <div className="Noticia-view">
+    <div className="View">
       {obj?
       <>
-      <h1>{obj.titulo}</h1>
-      <h2>{obj.descricao}</h2>
-      {obj.texto}
+      <img className="View-img" src={obj.foto} />
+      <div className="View-header">
+        <h1>{obj.titulo}</h1>
+        <h2>{obj.descricao}</h2>
+      </div>
+      <div className="View-text">
+        <Interweave content={obj.texto} />
+      </div>
       </>
       :
       <h1>Ocorreu um erro ao carregar a notícia</h1>
