3c1ede5474260f6b45d05f51183e5e0f44bd6c36
lauanS <lauan.s.souza@gmail.com>
2020-02-22 01:02:26 -0300

    Carregando dados da notícia

--- src/pages/public/Noticias/View/index.js
+++ src/pages/public/Noticias/View/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 
 import MyNavbar from '../../../../components/Navbar/index';
 import Footer from '../../../../components/Footer/index';
+import View from '../../../../components/Noticias/View'
+
+import {getNoticias} from '../../../../services'
+
 import '../styles.scss';
 
-export default function View(){
-  let { id } = useParams();
+export default function ViewNews(){
+  const { id } = useParams();
+  const [obj, setObj] = useState();
+
+  useEffect( () => {
+    async function load(){
+      const response = await getNoticias();
+      const objTemp = response.data.find(data => data.id === parseInt(id));
+      setObj(objTemp);
+    }
+    load();
+  }, [id]);
+
+  console.log(obj);
   return (
     <>  
     <div>
@@ -14,7 +30,7 @@ export default function View(){
         <MyNavbar initActive={2}/>
       </div>
       <div className="pagNoticia">
-        <h1>ABRIU A NOTICIA {id}</h1>       
+        <View obj={obj}/>
       </div>      
       <div className="footer">
         <Footer />
