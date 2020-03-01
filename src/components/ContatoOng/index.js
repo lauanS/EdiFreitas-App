d0f3aeb61e3e32767d6d66d63df222e650ed83bb
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-03-01 19:34:44 -0300

    finalizando informacoes de contato da ong

--- src/components/ContatoOng/index.js
+++ src/components/ContatoOng/index.js
@@ -5,47 +5,41 @@ import './styles.scss';
 import Facebook from '@material-ui/icons/Facebook';
 import Instagram from '@material-ui/icons/Instagram';
 
-export default function ContatoOng(props){
-    
-  
-    return (
-      <div className="contato-ong">
-        <div className="info">
-            <span className="txt1">Redes Sociais:</span>
-            
-            <br/>
+export default function ContatoOng(){
+  return (
+    <div className="contatoOng">
+      <div className="contatoOng__info">
+        <span className="contatoOng__txt1">Redes Sociais:</span>
+        <br/>
+        <div className="contatoOng__divIcons">
+          <a className="contatoOng__link" href="http://facebook.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
+            <Facebook className="contatoOng__icons" /> Facebook
+          </a>
 
-            <div className="row">
-                <a className="icons col-12 col-sm-6" href="http://facebook.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
-                    <Facebook className="icons" /> Facebook
-                </a>
-                <br/>
-                <a className="icons col-12 col-sm-6"  href="http://instagram.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
-                    <Instagram className="icons" /> Instagram
-                </a>
-            </div>
+          <a className="contatoOng__link" href="http://instagram.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
+            <Instagram className="contatoOng__icons" /> Instagram
+          </a>
         </div>
-
-
-        <div className="info">
-            <span className="txt1">Endereço:</span>
-            <br/>
-            <span className="txt2">Rua Três, N°12, Jardim Itapemirim - Sorocaba SP - CEP 18071-852</span>
-        </div>
-
-
-        <div className="info">
-            <span className="txt1">Fale conosco:</span>
-            <br/>
-            <span className="txt3">(15)98817-8399</span>
-        </div>
-
-        <div className="info">
-            <span className="txt1">Email:</span>
-            <br/>
-            <span className="txt3">ongedifreitas@gmail.com</span>
-        </div>
-        
       </div>
-    );
+
+      <div className="contatoOng__info">
+        <span className="contatoOng__txt1">Endereço:</span>
+        <br/>
+        <span className="contatoOng__txt2">Rua Três, N°12, Jardim Itapemirim - Sorocaba SP - CEP 18071-852</span>
+      </div>
+
+
+      <div className="contatoOng__info">
+        <span className="contatoOng__txt1">Telefone:</span>
+        <br/>
+        <span className="contatoOng__txt2">(15) 98817-8399</span>
+      </div>
+
+      <div className="contatoOng__info">
+        <span className="contatoOng__txt1">Email:</span>
+        <br/>
+        <span className="contatoOng__txt2">ongedifreitas@gmail.com</span>
+      </div>
+    </div>
+  );
 }
