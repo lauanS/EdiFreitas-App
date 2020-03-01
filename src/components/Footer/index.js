037c0674ebcebe65ca75ac93cd71f15565c5382a
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-02-29 21:29:33 -0300

    alterando informações do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,16 +10,16 @@ export default class Footer extends React.Component{
       <div className="foot">        
         <div className="foot__row">
           <a className="foot__link" href="http://facebook.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
-            <Facebook className="foot__icons" /> Facebook
+            <Facebook className="foot__icons" />
           </a>
           <a className="foot__link" href="http://instagram.com/ongedifreitas" target="_blank" rel="noopener noreferrer">
-            <Instagram className="foot__icons" /> Instagram
+            <Instagram className="foot__icons" />
           </a>
         </div>
 
         <br/>
         <div className="foot__row">
-          <p>Endereço - Rua Três, Nº 12, CEP 18071-303.</p>
+          <p>Rua Três, N° 12, Jardim Itapemirim, CEP 18071-852.</p>
         </div>
         <div className="foot__row">
           <p>Sorocaba - São Paulo - Brasil.</p>
@@ -27,7 +27,7 @@ export default class Footer extends React.Component{
 
         <br/>
         <div className="foot__row">
-          <p>{"ONG EdiFreitas, 2014 - "}{new Date().getFullYear()}</p>
+          <p>{"ONG EdiFreitas, 2015 - "}{new Date().getFullYear()}</p>
         </div>
 
       </div>
