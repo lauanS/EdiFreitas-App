63188df1a50a2a9534601b930380aae2a2e1dd4b
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-03-01 11:21:10 -0300

    alteracoes iniciais da home

--- src/pages/public/Inicio/index.js
+++ src/pages/public/Inicio/index.js
@@ -4,28 +4,27 @@ import Footer from '../../../components/Footer/index'
 import MyCarousel from '../../../components/MyCarrousel/index';
 import './styles.css';
 
-export default class Inicio extends React.Component{
-  render(){
-    return (
-      <div>
-        <div className="nav">
-          <MyNavbar initActive={0}/>
-        </div>
-
-        <br />
-        <br />
-
-        <div className="carousel">
-          <MyCarousel />
-        </div>
-
-        <br/>
-
-        <div>
-          <Footer />
-        </div>
-        
+export default function Inicio(){
+  return (
+    <div>
+      <div className="nav">
+        <MyNavbar initActive={0}/>
       </div>
-    );
-  }
+
+      <br />
+      <br />
+
+      <div className="carousel">
+        <MyCarousel />
+      </div>
+
+      <br/>
+
+      <div>
+        <Footer />
+      </div>
+      
+    </div>
+  );
+
 }
