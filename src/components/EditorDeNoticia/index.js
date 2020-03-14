aca8e94cae468760c9af6eb424c1dba056fd015a
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-03-14 17:02:51 -0300

    Corrigindo o resetField do editor de noticia

--- src/components/EditorDeNoticia/index.js
+++ src/components/EditorDeNoticia/index.js
@@ -42,6 +42,8 @@ export default function EditorDeNoticia(props){
     setSubtitle("");
     setTags("");
     setText("");
+    setImgBase64("");
+    setIsLoading(false);
   }
 
   function checkFields(){
